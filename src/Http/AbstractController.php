eb3db114773ec437c68378211dfcebea690f79f2
Marc MOSCA <marc.mosca@icloud.com>
2025-07-18 11:53:39 +0200

    Add json response and documentation

--- src/Http/AbstractController.php
+++ src/Http/AbstractController.php
@@ -4,23 +4,59 @@ namespace App\Http;
 
 use App\Views\TemplateEngine;
 
+/**
+ * This abstract class provides common functionality for handling HTTP requests and responses,
+ * including rendering HTML templates and returning JSON responses.
+ */
 abstract class AbstractController
 {
 
+	/**
+	 * @var TemplateEngine $templateEngine Template engine used to render views.
+	 */
 	private readonly TemplateEngine $templateEngine;
 
+	/**
+	 * @var Request $request Current HTTP request.
+	 */
 	public readonly Request $request;
 
+	/**
+	 * Initializes the template engine and request object.
+	 */
 	public function __construct()
 	{
 		$this->templateEngine = new TemplateEngine();
 		$this->request = new Request();
 	}
 
+	/**
+	 * Renders an HTML template with the given parameters and returns an HTTP response.
+	 *
+	 * @param string $template The name or path of the template to render.
+	 * @param array  $params Optional parameters to pass to the template.
+	 *
+	 * @return Response The HTTP response containing the rendered HTML.
+	 */
 	public function render(string $template, array $params = []): Response
 	{
 		$content = $this->templateEngine->render($template, $params);
 		$response = new Response($content, Response::HTTP_OK, ['Content-Type' => 'text/html']);
+		$response->send();
+		return $response;
+	}
+
+	/**
+	 * Returns a JSON response from an associative array.
+	 *
+	 * @param array $data The data to be returned as JSON.
+	 *
+	 * @return Response The HTTP response containing the JSON-encoded data.
+	 */
+	public function json(array $data): Response
+	{
+		$json = json_encode($data, JSON_PRETTY_PRINT);
+		$response = new Response($json, Response::HTTP_OK, ['Content-Type' => 'application/json']);
 		$response->send();
 		return $response;
 	}
